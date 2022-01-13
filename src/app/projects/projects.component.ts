a17585fd0391ae9bcfc253ebc50bfd4c112983d6
zhartig <zhartig1@gmail.com>
2022-01-12 22:38:55 -0500

    more experience slight view tweaks

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -22,6 +22,7 @@ export class ProjectsComponent implements OnInit {
         to compute the model down from weeks to a few hours.  Additionally, I developed a CI pipeline that compiles the
         matlab code so that it can be run without a license. At this point, we are generating models that use over 5 years
         of server time to generate the model in only 36 hours.`,
+
         `Additionally, I have worked to define the API schema that will be consumed by the public facing website, and a quarterly
         batch job, and implemented the skeleton of this into the service that will receive the requests.`
       ]
@@ -34,7 +35,9 @@ export class ProjectsComponent implements OnInit {
         `This is a POC project I worked on to build out an enterprise ready instance of NiFi ready for multi-tenant use.
         As a part of this effort, I researched into how to run NiFi in a clustered environment and connecting it to
         a centralized NiFi Registry for versioning and storage of flows.`,
+
         `The result of this was a set of Docker containers that are preconfigured to connect together.`,
+
         `I also helped to define how users would onboard to use the NiFi environment in a self-service way, and how
         deployed containers could connect to retrieve secrets needed for the configured flows.  Additionally, I helped
         to design the process to move NiFi flows from development to production environments.`
@@ -45,9 +48,11 @@ export class ProjectsComponent implements OnInit {
       employer: 'T. Rowe Price',
       time: 'February 2018 - Present',
       description: [
-        `describe product`,
-        `describe ML detection`,
-        `Starting in 2021, I became the technical lead for the development on this project.  As lead, my resposniblity
+        `TODO describe product`,
+
+        `TODO describe ML detection`,
+
+        `Starting in 2021, I became the technical lead for the development on this project.  As lead, my responsibility
         was to engage with the product owner to prioritize, refine, and break down work into stories that were then brought into
         my team's sprints.  I then delegated the stories as necessary, as well as helped architect the software solutions,
         and reviewing the code when the story is completed.`
@@ -58,6 +63,15 @@ export class ProjectsComponent implements OnInit {
       time: 'June 2017 - Present',
       employer: 'T. Rowe Price',
       description: [
+        `TODO sync`,
+
+        `TODO alerts`,
+
+        `TODO queue manager`,
+
+        `TODO websockets`,
+
+        `TODO IAM external access`
 
       ]
     },
@@ -69,10 +83,16 @@ export class ProjectsComponent implements OnInit {
         so when you have a new project or component, you only need to import the templates, and set environment variables specific to the project.
         I also created a script that enabled continuous deployment with the firm's deployment platform over a year before
         a similar feature was released by the deployment platform.`,
-        `Auto MAR`,
-        'Recently, a question and answer forum was added at T. Rowe Price for developers to ask questions that are specific' +
-        'to the company.  Since the product became generally available, I have become the highest rated user on the platform.',
-        'Splunk'
+
+        `I often do work not directly related to the projects that I am working on that help improve the process of development.
+        For example, I created a puppeteer script that automated the submission of access requests; the script takes in a JSON
+        document that describes what users and what roles to request.  It then automatically logs into the access site adds
+        all applicable items and submits the request.  Another example is a tool I published that automatically discovers what
+        AWS credentials a user has access to, and then fetches and refreshes the credentials before they expire.  This tool
+        has become so popular that it is now bundled into the default linux developer desktop.`,
+
+        `Recently, a question and answer forum was added at T. Rowe Price for developers to ask questions that are specific
+        to the company.  Since the product became generally available, I have become the highest rated user on the platform.`,
       ]
     },
   ]
@@ -85,10 +105,13 @@ export class ProjectsComponent implements OnInit {
       description: [
           `I run a few servers in my homelab right now.  Currently, my setup is an Ubuntu server, which runs many of my 
           other personal projects.  It is running a Plex server, a Minecraft server, a SFTP server, and my chat-bot right now.`,
+
           `The other main component to my homelab is a storage server.  It runs using Unraid OS, which makes it easy to add and replace drives.
           It currently has 12TB of storage, and can be mounted via NFS.  The data on the drives is automatically encrypted at rest.
           One upgrade I would like to implement on this server is so that the encrypted drives can be automatically unlocked at startup.`,
+
           `Occasionally, I have small servers running on Raspberry Pi when I have small programs to run.`,
+
           `In terms of future work, there are a few things I would like to do.  One is improving the network security;
           currently, it blocks all non-local access on port 22, and allows HTTPS traffic from any IP.  I would like to 
           add a way for me to VPN onto my network to manage locally, as well as installing a central firewall.  Second,
@@ -104,9 +127,11 @@ export class ProjectsComponent implements OnInit {
       description: [
           `This is an application I built to help comply with COVID restriction at my local dog park.  When the park 
           reopened, capacity was limited to 10 people at a time.`,
+
           `To help ensure people could enter the park when it opened, I built a simple site with an Angular front end, 
           and an express backend.  The site would allow each member to select a limited amount a reservations per day, 
           and enforce the 10 person max at a time.`,
+
           `View it on Github -- TODO make public & push backend`
       ]
     },
@@ -118,9 +143,11 @@ export class ProjectsComponent implements OnInit {
         `This is an application that I wrote to solve a problem with a group text for members of a local dog park.
           The Problem is that text message groups are limited to 20 participants, and we hit this limit for the group.
           There were many members who were unwilling or unable to install a 3rd party messaging app.`,
+
         `To solve this, I wrote a Spring Boot application that monitors an Gmail account via Google's API.
           Users would text the email address, and the application would then publish the message to all other users, 
           with info about who sent it.`,
+
         `View it on Github -- TODO make public`
       ]
     },
